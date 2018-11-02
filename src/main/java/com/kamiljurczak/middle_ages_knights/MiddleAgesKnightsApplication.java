db7a3294a7783ad7448432be6786bfd7795dc4d0
kamillj <kamil.jurczak@gmail.com>
2018-11-02 11:40:53 +0100

    @ComponentScan - wyszukiwanie komponentów

--- src/main/java/com/kamiljurczak/middle_ages_knights/MiddleAgesKnightsApplication.java
+++ src/main/java/com/kamiljurczak/middle_ages_knights/MiddleAgesKnightsApplication.java
@@ -1,14 +1,17 @@
 package com.kamiljurczak.middle_ages_knights;
 
+import com.kamiljurczak.component.TestComponent;
 import com.kamiljurczak.middle_ages_knights.domain.Castle;
+import com.kamiljurczak.middle_ages_knights.domain.Knight;
+import com.kamiljurczak.middle_ages_knights.domain.Quest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@ComponentScan({"com.kamiljurczak.middle_ages_knights", "com.kamiljurczak.component"})
+@ComponentScan(basePackageClasses = {Starter.class, Castle.class, Quest.class, Knight.class, TestComponent.class})
+//@ComponentScan({"com.kamiljurczak.middle_ages_knights", "com.kamiljurczak.component"})
 public class MiddleAgesKnightsApplication {
 
 	public static void main(String[] args) {
