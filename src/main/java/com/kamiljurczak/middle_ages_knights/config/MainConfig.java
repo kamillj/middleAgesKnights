e65997a7b8adb66b75831d3e3cc34e4c9b414e9f
kamillj <kamil.jurczak@gmail.com>
2018-12-06 20:00:44 +0100

    Dodanie Spring Security

--- src/main/java/com/kamiljurczak/middle_ages_knights/config/MainConfig.java
+++ src/main/java/com/kamiljurczak/middle_ages_knights/config/MainConfig.java
@@ -3,9 +3,15 @@ package com.kamiljurczak.middle_ages_knights.config;
 import com.kamiljurczak.middle_ages_knights.domain.repository.DBKnightRepository;
 import com.kamiljurczak.middle_ages_knights.domain.repository.InMemoryKnightRepository;
 import com.kamiljurczak.middle_ages_knights.domain.repository.KnightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class MainConfig {
 
     @Bean(name = "inMemoryKnightRepository")
@@ -18,5 +24,13 @@ public class MainConfig {
     @Profile("prod")
     public KnightRepository createDBRepo(){
         return new DBKnightRepository();
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("kamil").password("pw").roles("USER")
+                .and()
+                .withUser("admin").password("pw").roles("ADMIN");
     }
 }
